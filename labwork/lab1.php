cd9605cdd8f2e66ea4743d58073bfb4d625e136f
EC2 Default User <ec2-user@ip-172-31-83-115.ec2.internal>
2019-08-29 02:58:26 +0000

    Created lab1 file

---
+++ labwork/lab1.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head><title>My First PHP Page</title></head>
+    <body>
+        <h1>Leonor Diaz</h1>
+        <p>Add a paragraph here</p>
+        
+        <?php  
+            echo 'This is my PHP beautiful paragraph';
+        ?>
+
+</body>
+</html>
